93886a582532d1a20b870e8e69128b6676a65b4c
moalam7 <mohammedalamx@gmail.com>
2024-05-02 08:35:23 -0400

    dates

--- dates/dateScripts/date_to_string.js
+++ dates/dateScripts/date_to_string.js
@@ -8,3 +8,5 @@ console.log(d.toString());
 console.log(d.toDateString());
 
 console.log(d.toUTCString());
+
+console.log(d.toLocaleString());
---
+++ dates/dateScripts/more_date_conversions,.js
@@ -0,0 +1,12 @@
+let d = new Date("2024-05-01");
+console.log(d.toDateString());
+console.log(d.toISOString());
+
+"use strict";
+
+let birthday = new Date(1997, 2, 16);
+console.log(birthday.toLocaleString())
+
+let momBirthday = new Date();
+console.log(momBirthday.toLocaleString())
+console.log(momBirthday.toLocaleString())
